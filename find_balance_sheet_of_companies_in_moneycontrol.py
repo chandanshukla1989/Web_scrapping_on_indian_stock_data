a3af5e4b8fa3a5a72affbb09dba0ef7339dc073c
chandanshukla1989 <50393216+chandanshukla1989@users.noreply.github.com>
2019-06-14 09:01:24 +0000

    Update find_balance_sheet_of_companies_in_moneycontrol.py

--- find_balance_sheet_of_companies_in_moneycontrol.py
+++ find_balance_sheet_of_companies_in_moneycontrol.py
@@ -8,7 +8,7 @@ except ImportError:
 list_1=['2M','21C','I11','MI42','PI36','C98']
 i = 0
 #while i < len(list_1):
-query = "balance sheet 2M moneycontrol"
+query = "balance sheet moneycontrol"
 for j in search(query, tld="co.in", num=4, stop=1, pause=2): 
         print(j) 
     
